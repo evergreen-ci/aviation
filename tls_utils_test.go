afe7443e52158e99cad2b370b9112c09de95a84e
Kim Tao <kim.tao@mongodb.com>
2021-10-25 16:31:45 -0400

    Remove cert pool equality check since it panics and we don't use it anymore

--- tls_utils_test.go
+++ tls_utils_test.go
@@ -2,10 +2,7 @@ package aviation
 
 import (
 	"crypto/tls"
-	"crypto/x509"
-	"io/ioutil"
 	"path/filepath"
-	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -53,18 +50,6 @@ func TestGetClientTLSConfig(t *testing.T) {
 				require.NoError(t, err)
 				require.Len(t, conf.Certificates, 1)
 				assert.Equal(t, expectedCrt, conf.Certificates[0])
-
-				cp := x509.NewCertPool()
-				if runtime.GOOS != "windows" {
-					cp, err = x509.SystemCertPool()
-					require.NoError(t, err)
-				}
-				for _, ca := range test.cas {
-					ca, err := ioutil.ReadFile(ca)
-					require.NoError(t, err)
-					require.True(t, cp.AppendCertsFromPEM(ca))
-				}
-				assert.Equal(t, cp, conf.RootCAs)
 			}
 		})
 	}
