7fac7769abff6b612ddbbc0b67b8a021e017b130
julianedwards <julian.edwards@mongodb.com>
2021-10-26 11:40:49 -0400

    add back cert pool test

--- tls_utils_test.go
+++ tls_utils_test.go
@@ -2,7 +2,10 @@ package aviation
 
 import (
 	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
 	"path/filepath"
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -50,6 +53,18 @@ func TestGetClientTLSConfig(t *testing.T) {
 				require.NoError(t, err)
 				require.Len(t, conf.Certificates, 1)
 				assert.Equal(t, expectedCrt, conf.Certificates[0])
+
+				cp := x509.NewCertPool()
+				if runtime.GOOS != "windows" {
+					cp, err = x509.SystemCertPool()
+					require.NoError(t, err)
+				}
+				for _, ca := range test.cas {
+					ca, err := ioutil.ReadFile(ca)
+					require.NoError(t, err)
+					require.True(t, cp.AppendCertsFromPEM(ca))
+				}
+				assert.Equal(t, cp.Subjects(), conf.RootCAs.Subjects())
 			}
 		})
 	}
